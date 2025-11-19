5d15fb81868afa2465615fff2f01c0fff78bd86b
Saimon Lovell <staysynchronize@gmail.com>
2025-11-19 08:14:36 -0500

    adding location and weather details methods which will get data from the api response

--- app/services/weather_api_service/current.rb
+++ app/services/weather_api_service/current.rb
@@ -4,9 +4,16 @@ module WeatherApiService
       @zip_code = zip_code
     end
 
+    def location
+      get_weather["location"]
+    end
+
+    def weather_details
+      get_weather["current"]
+    end
+
     def get_weather
-      url_to_call = api_call_url(@zip_code)
-      api_call(url_to_call)
+      @get_weather ||= api_call(api_call_url(@zip_code))
     end
 
     # NOTE: This 3rd party service requires the api key to be exposed in the url.
