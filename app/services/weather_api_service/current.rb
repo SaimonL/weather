159d6b9119adf033637f4a047b04083b4c9adbbe
Saimon Lovell <staysynchronize@gmail.com>
2025-11-19 19:28:34 -0500

    commend and refactor

--- app/services/weather_api_service/current.rb
+++ app/services/weather_api_service/current.rb
@@ -4,7 +4,7 @@ module WeatherApiService
       @zip_code = zip_code
     end
 
-    def location
+    def location_details
       get_weather["location"]
     end
 
@@ -12,14 +12,27 @@ module WeatherApiService
       get_weather["current"]
     end
 
+    # Always cache API response. It gives developer more flexibility.
     def get_weather
-      @get_weather ||= api_call(api_call_url(@zip_code))
+      # Auto caches the data if not found in cache.
+      @get_weather ||=
+        Rails.cache.fetch(cache_key, expires_in: WEATHER_CACHE_TTL_MINUTES) do
+          api_call(api_call_url(@zip_code))
+        end
     end
 
     # NOTE: This 3rd party service requires the api key to be exposed in the url.
     # Unfortunately they do not accept api key in any other way.
     def api_call_url(zip_code)
       [ api_base_url, "current.json?key=#{WEATHER_API_KEY}&q=", zip_code ].join
+    end
+
+    def cache_key
+      [ :current, :weather,  @zip_code ].join("_")
+    end
+
+    def cached_data
+      Rails.cache.fetch(cache_key)
     end
   end
 end
