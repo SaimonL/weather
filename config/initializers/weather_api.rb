837f5881c1b620f7b3f2b77b0064f5dbf165a59b
Saimon Lovell <staysynchronize@gmail.com>
2025-11-20 15:26:33 -0500

    in test we don't need weather api key

--- config/initializers/weather_api.rb
+++ config/initializers/weather_api.rb
@@ -2,6 +2,6 @@ WEATHER_API_KEY = ENV["WEATHER_API_KEY"]
 WEATHER_API_VERSION = "v1".freeze
 WEATHER_CACHE_TTL_MINUTES = 30.minutes.freeze
 
-if !Rails.env.test && (WEATHER_API_KEY.nil? || WEATHER_API_KEY.empty?)
+if !Rails.env.test? && (WEATHER_API_KEY.nil? || WEATHER_API_KEY.empty?)
   raise "Please specify the WEATHER_API_KEY in the environment variable"
 end
